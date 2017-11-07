4781e97d8ad686a2223883d6f105ca7aec01c378
dfp2a <dfpauli@gmail.com>
2017-11-07 08:47:56 +0100

    Add files via upload

---
+++ src/application/Main.java
@@ -0,0 +1,35 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Label t = new Label();
+			t.setText("Halllo Welt");
+			VBox vbox= new VBox(t);
+			Parent root = vbox;
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setTitle("BugTrackerDemo");
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
