cf132de340658cae77b84ca50e1d8e5f5c7cd292
dfp2a <dfpauli@gmail.com>
2017-11-09 08:43:20 +0100

    Button hinzugefügt

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,14 +1,13 @@
 package application;
 	
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.stage.Stage;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
+import javafx.scene.layout.GridPane;
 
 
 public class Main extends Application {
@@ -17,8 +16,14 @@ public class Main extends Application {
 		try {
 			Label t = new Label();
 			t.setText("Hallo Welt");
-			VBox vbox= new VBox(t);
-			Parent root = vbox;
+			Button b = new Button("Close");
+			GridPane grid= new GridPane();
+			grid.setAlignment(Pos.CENTER);
+			grid.prefWidthProperty().bind(primaryStage.widthProperty());
+			grid.prefHeightProperty().bind(primaryStage.heightProperty());
+			grid.add(t, 0, 0);
+			grid.add(b, 0, 1);
+			Parent root = grid;
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setTitle("BugTrackerDemo");
